0bda15a72ed2aa879102b91fa216444f4fb068b7
TerryX <terryxlife@gmail.com>
2016-10-20 16:51:55 +0800

    Change test CheckBoxWithLabel

--- examples/enzyme/__tests__/CheckboxWithLabel-test.js
+++ examples/enzyme/__tests__/CheckboxWithLabel-test.js
@@ -3,16 +3,19 @@
 /* eslint-disable no-unused-vars */
 
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import CheckboxWithLabel from '../CheckboxWithLabel';
 
 it('CheckboxWithLabel changes the text after click', () => {
   // Render a checkbox with label in the document
-  const checkbox = shallow(
+  const checkbox = mount(
     <CheckboxWithLabel labelOn="On" labelOff="Off" />
   );
 
   expect(checkbox.text()).toEqual('Off');
+
+  //this causes stack overflow
+  expect(checkbox.find('.abc')).toBe(true)
 
   checkbox.find('input').simulate('change');
 
